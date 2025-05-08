0975fbbc4b6f40145be499ac3297d4b204a826eb
Melezwa26 <phalosenzwa@gmail.com>
2025-05-08 13:30:41 +0200

    ClosurePlus_An__Rc_RefCell_comparison

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
-}
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     vec![]
+// }
 
 // #[cfg(test)]
 // mod tests {
@@ -21,7 +21,7 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 // }
 
 
-// Working with environment variables - adding an extra feature for a case INsensitive searching the user can turn on via an environment variable - via the CLI..
+//  Working with environment variables - adding an extra feature for a case INsensitive searching the user can turn on via an environment variable - via the CLI..
 
 // Writing a Failing Test for the Case-Insensitive 'search' Function
 
@@ -29,36 +29,201 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 // 'search_case_insensitive' function and rename our old test from 'one_result' to 'case_sensitive' to clarify the differences btn the two tests:
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape. ";
+//     #[test]
+//     fn case_sensitive() {
+//         let query = "duct";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Duct tape. ";
 
-        assert_eq!(vect!["safe, fast, productive."], search(query, contents)); 
+//         assert_eq!(vec!["safe, fast, productive."], search(query, contents)); 
 
-    }
+//     }
 
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
+//     #[test]
+//     fn case_insensitive() {
+//         let query = "rUsT";
+//         let contents = "\
         
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me. ";
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Trust me. ";
 
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
+//         assert_eq!(
+//             vec!["Rust:", "Trust me."],
+//             search_case_insensitive(query, contents)
+//         );
+//     } 
+// Note that we've edited the old test's contents too. We've added a nwe line with the text 'Duct tape'. Using a capital "D" that should't match the query 'duct'
+// when we're searching in a case-sensitive amnner. Changing the old test in this way helps ensure that we don't accidentally break the case-sensitive sarch functionality that we've already implemented.
+// This test shouldn pass now and should continue to pass as we work on the case-insensitive search. 
+// The new test for the 'case-INsensitive' search uses 'rUsT' as its query. In the 'search_case_insensitive' function we're about to add , the query 'rUsT' should match the line containing "Rust:" with a capital R and match the line "Trust me." even though both have different casing from the query. This is our failing test, and it will fail to compile because we have'nt yet defined the 'search_case_insensitive' function.
+ 
+// IMPLEMENTING the 'search_case_insensitive' FUNCTION
+
+// Filename: src/lib.rs
+    
+//     pub fn search_case_insensitive<'a>(
+//         query: &str,
+//         contents: &'a str,       
+//     ) -> Vec<&'a str> {
+//         let query = query.to_lowercase();
+//         let mut results = Vec::new();
+
+//         for line in contents.lines() {
+//             if line.to_lowercase().contains(&query) {
+//                 results.push(line);
+//             }
+//         }
+
+//         results
+//     } // First, we lowecase the 'query' string and store it in a new variable with the same name, shadowing the original.
+// //     // Calling 'to_lowercase' on the query is neccessary so that no matter whether the user's query is ( "rust, "RUST", "Rust", or "rUsT") we'll treat the query as if it were "rust" and be insensitive to the case. While to 'lowercase' will handle basic Unicode, it won't be 100% accurate.If we were writing a real application, we would need to do a bit work here, but this is about environmental variables, not Unicode, so we'll let it slide 
+// //     // Note that 'query' is now a String rather than a string slice because calling 'to_lowercase' creates new data rather than referencing existing data. Say the query is "rUsT", as an example: that string slice doesn't contain a lowercase 'u' or 't' for to use, so we have to allocate a new String "rust". When we pass 'query' as an argument to the 'contains' method now, we need to add an ampersand because the signature of 'contains' is defined to take a string slice.
+// //     // Next, we add a call to 'to_lowercase' on each 'line' to lowercase all characters. Nowthat we've converted 'line' and 'query' to lowercase, we'll find mathces no matter what the case of the query is.
+
+// }
+
+// // lib.rs
+
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     contents
+//         .lines()
+//         .filter(|line| line.contains(query))
+//         .collect()
+// }
+
+// pub fn search_case_insensitive<'a>(
+//     query: &str,
+//     contents: &'a str,
+// ) -> Vec<&'a str> {
+//     let query = query.to_lowercase();
+//     let mut results = Vec::new();
+
+//     for line in contents.lines() {
+//         if line.to_lowercase().contains(&query) {
+//             results.push(line);
+//         }
+//     }
+
+//     results
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn case_sensitive() {
+//         let query = "duct";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Duct tape.";
+
+//         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+//     }
+
+//     #[test]
+//     fn case_insensitive() {
+//         let query = "rUsT";
+//         let contents = "\
+// Rust:
+// safe, fast, productive.
+// Pick three.
+// Trust me.";
+
+//         assert_eq!(
+//             vec!["Rust:", "Trust me."],
+//             search_case_insensitive(query, contents)
+//         );
+//     }
+// }
+
+
+// Calling the 'search_case_insensitive' function from the 'run' function.First we create a configuration option to the 'Config' struct to switch between the case-sensitive and 
+// case-insensitive search. This field addition will cause compilation errors...
+
+// Filename: src/lib.rs
+
+// use std::env;
+// use std::error::Error;
+// use std::fs;
+
+    // pub struct Config {
+    //     pub query: String,
+    //     pub file_path: String,
+    //     pub ignore_case: bool,
+    // }
+
+// We added the 'ignore_case' field that holds the a Boolean. Next, we need the 'run' function to check the 'ignore_case' field's value and use that to decide whether to call the 'search' function or the 'search_case_insensitive' function.
+// Thii still won't compile;
+
+//Filename: src/lib.rs
+
+use std::error::Error;
+use std::env;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query)) 
+        .collect()
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,150 @@
-use std::env;
-use std::fs;
+// use std::env;
+// use std::process;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+// use minigrep::Config; // from lib.rs
+// use minigrep::run;
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        std::process::exit(1);
-    });
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
 
-    let contents = fs::read_to_string(&config.file_path)
-        .expect("Should have been able to read the file");
+//     let config = Config::build(&args).unwrap_or_else(|err| {
+//         eprintln!("Problem parsing arguments: {}", err);
+//         std::process::exit(1);
+//     });
 
-    let results = search(&config.query, &contents);
-
-    for line in results {
-        println!("{}", line);
-    }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
- }
-
-fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
-
-}
-
-
-// OR....alternative search fun is below
-
-// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-//     let mut results = Vec::new();
-
-//     for line in contents.lines() {
-//         if line.contains(query) {
-//             results.push(line);
-//         }
+//     if let Err(e) = run(config) {
+//         eprintln!("Application error: {e}");
+//         process::exit(1);
 //     }
 
-//     results
-// }
+// 13.1 Capturing the environment with Closures
+
+    #[derive(Debug, PartialEq, Copy, Clone)]
+    enum ShirtColour {
+        Red,
+        Blue,
+    }
+
+    struct Inventory {
+        shirts: Vec<ShirtColour>,
+    }
+
+    impl Inventory {
+        fn giveaway(&self, user_preference: Option<ShirtColour>) -> ShirtColour {
+            user_preference.unwrap_or_else(|| self.most_stocked())
+        }
+
+        fn most_stocked(&self) -> ShirtColour {
+            let mut num_red = 0;
+            let mut num_blue = 0;
+
+            for colour in &self.shirts {
+                match colour {
+                    ShirtColour::Red => num_red += 1,
+                    ShirtColour::Blue => num_blue += 1,
+                }
+            }
+            if num_red > num_blue {
+                ShirtColour::Red
+            } else {
+                ShirtColour::Blue
+            }
+        }
+    }
+//}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColour::Blue, ShirtColour::Red, ShirtColour::Blue],
+
+    };
+    
+    let user_pref1 = Some(ShirtColour::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+    // Shirt company giveaway situation
+    // The closure captures an immutable reference to the 'self' "Inventory" instance and passes it with passes it with the code we specify to the 
+    // with the cpde we specify to the 'unwrap_or_else' method. Functions, on the other hand, are not able to capture their environment in this way.
+
+    // CAPTURING References OR Moving Ownership:
+
+    // Closures can capture values from their environment in 3 ways (1 borrowing immutably, 2 borrowing mutable, 3 and taking ownership)
+    // The closure will decide which of these to use based on what the body of the function does with the captured values.
+    
+   // fn main() {
+        let list = vec![1, 2, 3];
+        println!("Before defning closure: {list:?}");
+
+        let only_borrows = || println! ("From closure: {list:?}");
+
+        println!("Before calling closure: {list:?}");
+        only_borrows();
+        println!("After calling closure: {list:?}");
+   //} 
+   // Because we can have multiple immutable references to 'list' at the same time, 'list' is still accessible from the code before the closure definition, after the closure definintion but before tyhe closure is called, and after the closure is called. code compiles and runs. 
+
+
+   // Next, we change the closure body so that it adds an element to the 'list' vector. The closure now captures a mutable reference:
+
+   // fn main() {
+        let mut list = vec![1, 2, 3];
+        println!("Before defining closure: {list:?} ");
+
+        let mut borrows_mutably = || list.push(7);
+
+        borrows_mutably();
+        println!("After calling closure: {list:?}");
+   //}
+
+   // Note; there's no longer a 'println!' between the definintion and the call of the 'borrows_mutably' closure: when 'borrows_mutably' 
+   // is defined, it captures a mutable reference to list. We don't use a closure again after the closure is called, so the mutable borrow ends. Between the closure and the closure call, an immutable borrow to print isn't allowed because no other borrows are allowed when there's mutable borrow. 
+   // If you want to force the closure to take ownership of the values it uses in the environment even though the body of the closure doesn’t strictly need ownership, you can use the move keyword before the parameter list.
+    // If you want to force the closure to take ownership of the values it uses in the environment even though the body of the closure doesn’t strictly need ownership, you can use the move keyword before the parameter list.
+    // This technique is mostly useful when passing a closure to a new thread to move the data so that it’s owned by the new thread.
+
+
+    // BIG IDEA: comparing Closure with Rc<RefCell<Vec<i32>>> :
+
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    // fn main() {
+        let list = Rc::new(RefCell::(vec![1, 2, 3]));
+        println!("Before defining closure: {:?}", list.borrow());
+
+        let list_clone = Rc::clone(&list); // CREATING a second SHARED owner, now BOTH main and closure own list.
+        let modifies_list = move || {
+            list_clone.borrow_mut().push(4);
+            println!("From closure (after mutation): {:?}", list_clone.borrow());
+        };
+
+        println!("Before calling closure: {:?}", list.borrow());
+        modifies_list(); // calling the closure, pushes 4 into the vector
+        println!("After calling closure: {:?}", list.borrow());
+    //}
+
+    // Rc<T> : allows MULTIPLE OWNERS (reference counted)
+    // RefCell<T> : Allows MUTABLE BORROWS AT RUNTIME, even when the outer structure is immutable
+    // This setup enables cloning and MUTATING the vector inside the closure.
+
+    // The CLOSURE:
+
+    // let modifies_list = move || {
+    //    list_clone.borrow_mut().push(4);
+    //    println!("From closure (after mutation): {:?}", list_clone.borrow());
+    
+
+    // - move : Needed to transfer ownership of 'list_clone' into the closure.
+    // - borrow_mut() : gives a mutable reference to the vector (checked at runtime).
+    // - Closure modifies the vector --- something the original code couldn't do
+}
